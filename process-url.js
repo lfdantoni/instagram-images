1e33d799c84a66e583da33efdec2b0e0524707bc
lfdantoni <lfdantoni@gmail.com>
2021-03-28 20:18:21 -0300

    .

--- process-url.js
+++ process-url.js
@@ -29,7 +29,7 @@ exports.getInstImgUrl = async (instagramImgLink) => {
         parse = parse.substr(0, parse.length - 1);
   
         const jsonData = JSON.parse(parse);
-        console.log(jsonData)
+        console.log(jsonData.entry_data)
         if(checkCorrectInfo(jsonData)) {
           const mediaData = jsonData.entry_data.PostPage[0].graphql.shortcode_media;
           console.log(mediaData)
