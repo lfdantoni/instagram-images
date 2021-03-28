76e12ea7f9042acd45829fe862a99dfabebba213
lfdantoni <lfdantoni@gmail.com>
2021-03-28 19:31:50 -0300

    Add backend logic

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { getInstImgUrl } = require('./process-url');
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).send('All is ok2!')
+})
+
+app.post('/url', async (req, res) => {
+  const {instUrl} = req.body;
+  console.log(req.body)
+  try {
+    const imgUrls = await getInstImgUrl(instUrl);
+    res.status(200).send({imgUrls})
+  } catch (error) {
+    res.status(500).send({error})
+  }
+  
+  
+})
+
+app.listen(port, ()  => console.log('Server is running on port %s', port))
---
+++ backend/process-url.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+
+exports.getInstImgUrl = async (instagramImgLink) => {
+
+  // const url = 'https://images' + ~~(Math.random() * 3333) + '-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=' + instagramImgLink;
+  
+  return axios.get(instagramImgLink)
+    .then(resp => {
+      const {data} = resp;
+      try {
+        const shareDataPos = data.indexOf('<script type="text/javascript">window._sharedData = ');
+        let parse = data.substr(shareDataPos, data.length - shareDataPos);
+        parse = parse.split('<script type="text/javascript">window._sharedData = ')[1];
+        parse = parse.split('</script>')[0];
+        parse = parse.substr(0, parse.length - 1);
+  
+        const jsonData = JSON.parse(parse);
+
+        if(checkCorrectInfo(jsonData)) {
+          const mediaData = jsonData.entry_data.PostPage[0].graphql.shortcode_media;
+          console.log(mediaData)
+          if(mediaData.edge_sidecar_to_children) {
+            const imgs = mediaData.edge_sidecar_to_children.edges;
+  
+            const results = imgs.map(i => i.node.display_url);
+            return Promise.resolve(results);
+          }
+
+          return Promise.resolve(
+            [mediaData.display_url]
+          );
+        }
+      } catch (error) {
+        Promise.reject(error)
+      }
+    })
+  
+}
+
+const checkCorrectInfo = (jsonData) => !!(jsonData 
+  && jsonData.entry_data
+  && jsonData.entry_data.PostPage
+  && jsonData.entry_data.PostPage[0]
+  && jsonData.entry_data.PostPage[0].graphql
+  && jsonData.entry_data.PostPage[0].graphql.shortcode_media);
