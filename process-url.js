5d421d2d8935c22c1a99da94fa6185e284134791
lfdantoni <lfdantoni@gmail.com>
2021-03-28 21:05:47 -0300

    .

--- process-url.js
+++ process-url.js
@@ -6,18 +6,6 @@ exports.getInstImgUrl = async (instagramImgLink) => {
   
   return axios.get(
     url
-      // ,{
-      //   headers: {
-      //     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
-      //     "Accept-Encoding": "gzip, deflate", 
-      //     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8", 
-      //     "Dnt": "1", 
-      //     "Host": "instagram.com", 
-      //     "Upgrade-Insecure-Requests": "1", 
-      //     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36", 
-      //     "X-Amzn-Trace-Id": "Root=1-5ee7bae0-82260c065baf5ad7f0b3a3e3"
-      //   }
-      // }
     )
     .then(resp => {
       const {data} = resp;
